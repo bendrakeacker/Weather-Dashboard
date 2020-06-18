770ce65effb9ef7e3cd56857ec0be6ac7711a98b
Ben Acker <bda5464@nyu.edu>
2020-06-17 21:16:42 -0400

    revised js file

--- wthr.js
+++ wthr.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 //Variables
 var apiKey = "5f23293254a52c2562b57e712b10f79e";
 var dayCnt = "6";
+var cityArr = JSON.parse(localStorage.getItem("cities")) || [];
 
 //Functions
 $('#srchBtn').on('click', function() {
@@ -11,14 +12,19 @@ $('#srchBtn').on('click', function() {
     event.preventDefault();
     var srchCity = "";
     srchCity = $('#searchCity').val().trim(); 
-    
+
+
     function runQuery(srchCity) {
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + srchCity + "&cnt=" + dayCnt + "&appid=" + apiKey;
+        cityArr.push({
+            cityName: srchCity
+         });
+        localStorage.setItem("cities", JSON.stringify(cityArr));
         $.ajax({
             url: queryURL,
             method: "GET"
-            
         }).then(function (weatherData){
+            saveCity();
             for(var i = 0; i < 6; i++) {
                 var dayNumber = {
                     // day = moment().add(i, 'days').format('l');
@@ -30,38 +36,56 @@ $('#srchBtn').on('click', function() {
                 };
                 dayArr.push(dayNumber);
             }
+            var lon = weatherData.city.coord.lon;
+            var lat = weatherData.city.coord.lat;
+            queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + apiKey;
+
+        $.ajax({
+            
+            url: queryURL,
+            method: 'GET'
+        }).then(function (weatherData) {
+            var uvi = weatherData.daily[0].uvi;
+            $('#srchUVI').html(uvi);
+        })
             display(dayArr);
         });
     }
 
+
     function display(dayArr) {
         $('#srchCity').html(srchCity);
-        $('#srchTemp').html(dayArr[0].temp);
-        $('#srchHumid').html(dayArr[0].humidity);
-        $('#srchWind').html(dayArr[0].windSpeed);
-        $('#srchIcon').html(dayArr[0].icon);
+        $('#srchTemp').html("Temp: " + (dayArr[0].temp).toFixed(2));
+        $('#srchHumid').html("Humidity: " + (dayArr[0].humidity));  
+        $('#srchWind').html("Wind Speed: " + (dayArr[0].windSpeed));
+        $('#srchIcon').html((dayArr[0].icon));
+       
         
         for(var i = 1; i <= 5; i++) {
             var dayTemp = "#day" + i + "Temp";
-            $(dayTemp).html(dayArr[i].temp);
+            $(dayTemp).html("Temp: " + (dayArr[i].temp).toFixed(2));
 
             var dayHumid = "#day" + i + "Humid";
-            $(dayHumid).html(dayArr[i].humidity);
+            $(dayHumid).html("Humidity: " + (dayArr[i].humidity));
 
             var dayWind = "#day" + i + "Wind";
-            $(dayWind).html(dayArr[i].windSpeed);
+            $(dayWind).html("Wind Speed: " + (dayArr[i].windSpeed));
 
             var dayIcon = "#day" + i + "Icon";
             $(dayIcon).html(dayArr[i].icon);
-        }
-        
+        }   
     }
     runQuery(srchCity);
-    });
+    })
 
+    function saveCity() {
+        for(var i = 15; i > 0; i--) {
+            $('#btn' + i).html($('#btn' + (i-1)).html());
+        }
+    $('#btn1').html(srchCity)
+    }
+    
 });
 
 
     
-
-
